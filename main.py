6bf833c019c314fb25b66c5889c7baae1bd0ff76
makky0620 <b1013059@gmail.com>
2019-09-28 13:25:22 +0900

    testの追加

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    hello = "Hello world"
+    return hello
+
+
+if __name__ == "__main__":
+    app.run()
+
+
 def plus(x: int, y: int) -> int:
     return x + y
 
